5f969c802885fa87b97a193197b1e30c1486ab0d
Vital Wang <vital.wang@hotmail.com>
2020-03-03 10:59:41 +0100

    update install and gitignore files

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ ln -s ${BASEDIR}/vim/ ~/.vim
 # zsh
 ln -s ${BASEDIR}/zshrc ~/.zshrc
 
+# i3
+ln -s ${BASEDIR}/i3/config ~/.config/i3/config
+ln -s ${BASEDIR}/i3/i3status.conf ~/.config/i3status/config
+
 # git
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
