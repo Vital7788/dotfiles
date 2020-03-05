86864382810131840dbbb71ac49de484fc59e70d
Vital Wang <vital.wang@hotmail.com>
2020-03-05 16:29:11 +0100

    replace i3status with i3blocks

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
-ln -s ${BASEDIR}/vim/ ~/.vim
+ln -s ${BASEDIR}/vim ~/.vim
 
 # zsh
 # https://github.com/trapd00r/LS_COLORS
 ln -s ${BASEDIR}/zshrc ~/.zshrc
 
 # i3
-ln -s ${BASEDIR}/i3/config ~/.config/i3/config
-ln -s ${BASEDIR}/i3/i3status.conf ~/.config/i3status/config
+ln -s ${BASEDIR}/i3 ~/.config/i3
+ln -s ${BASEDIR}/i3blocks ~/.config/i3blocks
 
 # git
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
