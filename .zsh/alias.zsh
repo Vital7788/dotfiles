a43eb03013d6d7c0ea8a1c25887aaa771e33d66f
Vital Wang <vital.wang@hotmail.com>
2022-05-14 06:45:59 +0200

    fix faulty alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -8,7 +8,7 @@ for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-alias keychain="keychain --dir $XDG_RUNTIME_DIR/keychain $HOME/.ssh/id_*~$HOME/.ssh/id_*.pub"
+alias keychain="keychain --absolute --dir $XDG_RUNTIME_DIR/keychain $HOME/.ssh/id_*~$HOME/.ssh/id_*.pub"
 
 alias wget="wget --hsts-file=$XDG_STATE_HOME/wget-hsts"
 
