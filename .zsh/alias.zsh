c499e6d2dcbd84c123336ed31c2d2a4d98f2b7fe
Vital Wang <vital.wang@hotmail.com>
2024-03-01 16:30:02 +0100

    use cfg for dotfiles alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -6,7 +6,7 @@ alias d='dirs -v'
 alias dirs='dirs -v'
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 alias keychain="keychain --absolute --dir $XDG_RUNTIME_DIR/keychain $HOME/.ssh/id_*~$HOME/.ssh/id_*.pub"
 
