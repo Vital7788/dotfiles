bfab9efed59d9894a1fed1e9ed0100d29312842e
Vital Wang <vital.wang@hotmail.com>
2024-04-04 15:26:14 +0200

    vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,3 +20,22 @@ require("lazy").setup("plugins", {
     notify = false,
   },
 })
+
+-- set max width of lsp/diagnostic floating windows to 80
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.max_width= opts.max_width or 80
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- diagnostics
+vim.diagnostic.config({
+  underline = false,
+  virtual_text = true,
+  float = {
+    source = "always",
+  },
+})
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
--- .vim/after/ftplugin/python.vim
+++ .vim/after/ftplugin/python.vim
@@ -4,7 +4,3 @@ nnoremap <buffer> <Leader>r
 let b:undo_ftplugin .= '| nunmap <buffer> <Leader>r'
 
 let b:ale_python_flake8_options = '--config=$HOME/.config/flake8'
-
-if has('nvim')
-    let b:ale_linters_ignore = ['pyright', 'pylsp']
-endif
