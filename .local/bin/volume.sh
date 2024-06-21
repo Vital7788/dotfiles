7ec8da6038eab2ba4081e1f2e3a0fc9863a2a555
Vital Wang <vital.wang@hotmail.com>
2024-06-21 20:28:53 +0200

    update volume script

--- .local/bin/volume.sh
+++ .local/bin/volume.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -e -u -o pipefail
+
 volume=1
-sink_bus=$(pactl list sinks | grep $(pactl get-default-sink) -B4 -A55 | grep "device.bus =")
-active_port=$(pactl list sinks | grep $(pactl get-default-sink) -B4 -A55 | grep "Active Port:")
+active_sink=$(pactl list sinks | sed -n '/State: RUNNING/,/^$/p')
+if [[ -z $active_sink ]]; then
+    active_sink=$(pactl list sinks | sed -n "/$(pactl get-default-sink)/,/^$/p")
+fi
+
+sink_bus=$(echo "$active_sink" | grep "device.bus =")
+active_port=$(echo "$active_sink" | grep "Active Port:")
 if [[ $sink_bus =~ pci && $active_port =~ speaker ]]; then
     volume=5
 fi
@@ -12,5 +19,5 @@ if [[ $1 = dec ]]; then
     mod=-
 fi;
 
-
-pactl set-sink-volume @DEFAULT_SINK@ $mod${volume}%
+sink_name=$(echo "$active_sink" | grep "Name:" | cut -d ' ' -f 2)
+pactl set-sink-volume $sink_name $mod${volume}%
