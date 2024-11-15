e2305ff73d5d149bcb7d4dbe119bc0485ace8d41
Vital Wang <vital.wang@hotmail.com>
2024-11-15 14:28:44 +0100

    patch git switch autocompletion

--- .zsh/command.zsh
+++ .zsh/command.zsh
@@ -25,3 +25,22 @@ manopt() {
   [[ $opt == -* ]] || { (( ${#opt} == 1 )) && opt="-$opt" || opt="--$opt"; }
   man "$cmd" | col -b | awk -v opt="$opt" -v RS= '$0 ~ "(^|,)[[:blank:]]+" opt "([[:punct:][:space:]]|$)"'
 }
+
+# Clean up patch file
+scrub_patch() {
+	sed -i \
+		-e '/^index /d' \
+		-e '/^new file mode /d' \
+		-e '/^Index:/d' \
+		-e '/^=========/d' \
+		-e '/^RCS file:/d' \
+		-e '/^retrieving/d' \
+		-e '/^diff/d' \
+		-e '/^Files .* differ$/d' \
+		-e '/^Only in /d' \
+		-e '/^Common subdirectories/d' \
+		-e '/^deleted file mode [0-9]*$/d' \
+		-e '/^+++/s:\t.*::' \
+		-e '/^---/s:\t.*::' \
+		"$@"
+}
