0e3e356ee10c9abf3d04fd758012fb5c5b8d16e1
Vital Wang <vital.wang@hotmail.com>
2024-12-19 09:42:15 +0100

    nvim fzf quickfix

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -43,7 +43,13 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       -- calling `setup` is optional for customization
-      require('fzf-lua').setup({})
+      require('fzf-lua').setup({
+        keymap = {
+            fzf = {
+                ["ctrl-q"] = "select-all+accept",
+            },
+        },
+      })
     end,
     keys = {
         { '<localleader>f', function() require('fzf-lua').files() end },
