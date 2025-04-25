c70b09266e0eda3706380aec06fbcf4bc31144f6
Vital Wang <vital.wang@sigasi.com>
2025-04-25 17:16:10 +0200

    river + nvim vscode

--- .config/nvim/vscode.lua
+++ .config/nvim/vscode.lua
@@ -1,33 +1,36 @@
+local vscode = require('vscode')
+local keymap = vim.api.nvim_set_keymap
+local function action(cmd)
+  return string.format("<cmd>lua require('vscode').action('%s')<CR>", cmd)
+end
 
-  local vscode = require('vscode')
-  local keymap = vim.api.nvim_set_keymap
-  local function action(cmd)
-    return string.format("<cmd>lua require('vscode').action('%s')<CR>", cmd)
-  end
+-- list of potentially useful VS Code commands
+-- 'references-view.findReferences' -- language references
+-- 'workbench.actions.view.problems' -- language diagnostics
+-- 'editor.action.goToReferences'
+-- 'editor.action.rename'
+-- 'editor.action.formatDocument'
+-- 'editor.action.refactor' -- language code actions
+-- --
+-- 'workbench.action.findInFiles' -- use ripgrep to search files
+-- 'workbench.action.toggleSidebarVisibility'
+-- 'workbench.action.toggleAuxiliaryBar' -- toggle docview (help page)
+-- 'workbench.action.togglePanel'
+-- 'workbench.action.showCommands' -- find commands
+-- 'workbench.action.quickOpen' -- find files
+-- 'workbench.action.terminal.toggleTerminal' -- terminal window
+-- --
+-- 'editor.action.formatSelection'
+-- 'editor.action.refactor'
+-- 'workbench.action.showCommands'
 
-  -- list of potentially useful VS Code commands
-  -- 'references-view.findReferences' -- language references
-  -- 'workbench.actions.view.problems' -- language diagnostics
-  -- 'editor.action.goToReferences'
-  -- 'editor.action.rename'
-  -- 'editor.action.formatDocument'
-  -- 'editor.action.refactor' -- language code actions
-  -- --
-  -- 'workbench.action.findInFiles' -- use ripgrep to search files
-  -- 'workbench.action.toggleSidebarVisibility'
-  -- 'workbench.action.toggleAuxiliaryBar' -- toggle docview (help page)
-  -- 'workbench.action.togglePanel'
-  -- 'workbench.action.showCommands' -- find commands
-  -- 'workbench.action.quickOpen' -- find files
-  -- 'workbench.action.terminal.toggleTerminal' -- terminal window
-  -- --
-  -- 'editor.action.formatSelection'
-  -- 'editor.action.refactor'
-  -- 'workbench.action.showCommands'
-
-  vim.keymap.set('n', '<Leader>rl', action('workbench.action.reloadWindow'))
-  vim.keymap.set('n', '<Leader>fm', action('workbench.action.formatDocument'))
-  vim.keymap.set('n', ',f', action('workbench.action.quickOpen'))
-  vim.keymap.set('n', ',s', action('workbench.action.findInFiles'))
-  vim.keymap.set('n', 'gr', action('editor.action.goToReferences'))
-  vim.keymap.set('n', '<Leader>rn', action('editor.action.rename'))
+vim.keymap.set('n', '<Leader>rl', action('workbench.action.reloadWindow'))
+vim.keymap.set('n', '<Leader>fm', action('workbench.action.formatDocument'))
+vim.keymap.set('n', ',f', action('workbench.action.quickOpen'))
+vim.keymap.set('n', ',s', action('workbench.action.findInFiles'))
+vim.keymap.set('n', 'gr', action('editor.action.goToReferences'))
+vim.keymap.set('n', '<Leader>rn', action('editor.action.rename'))
+vim.keymap.set('n', ',b', action('workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup'))
+vim.keymap.set('n', '[b', action('workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup'))
+vim.keymap.set('n', ']b', action('workbench.action.quickOpenLeastRecentlyUsedEditorInGroup'))
+vim.keymap.set('n', ',o', action('outline.focus'))
--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -5,7 +5,7 @@ export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
-export XDG_CURRENT_DESKTOP="sway"
+# export XDG_CURRENT_DESKTOP="sway"
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/opt/cuda/extras/CUPTI/lib64"
 
