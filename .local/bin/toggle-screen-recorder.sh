c5ad6819e536543ff100ae30b26d4f07c93b3239
Vital Wang <vital.wang@hotmail.com>
2025-10-20 10:42:16 +0200

    git claim

--- .local/bin/toggle-screen-recorder.sh
+++ .local/bin/toggle-screen-recorder.sh
@@ -6,6 +6,7 @@ status=$?
 if [ $status != 0 ]
 then
   wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4') -o DP-1;
+  # wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4') -g "1508,350 2492x1227";
 else
   pkill --signal SIGINT -x wf-recorder
 fi;
