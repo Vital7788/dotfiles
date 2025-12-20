252de753347601a1c869933e87115f46a9f03e7f
Vital Wang <vital.wang@hotmail.com>
2025-12-20 22:18:18 +0100

    nvim diagnostics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,18 +33,19 @@ require("lazy").setup("plugins", {
 
 -- diagnostics
 vim.diagnostic.config({
-  underline = true,
-  virtual_text = true,
   severity_sort = true,
-  float = {
-    source = true,
+  float = { border = 'rounded', source = true },
+  virtual_text = { source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.WARNING },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
   },
 })
-
-vim.fn.sign_define("DiagnosticSignError", {text="", texthl="DiagnosticSignError", numhl="DiagnosticSignError"})
-vim.fn.sign_define("DiagnosticSignWarn", {text="", texthl="DiagnosticSignWarn", numhl="DiagnosticSignWarn"})
-vim.fn.sign_define("DiagnosticSignInfo", {text="", texthl="DiagnosticSignInfo", numhl="DiagnosticSignInfo"})
-vim.fn.sign_define("DiagnosticSignHint", {text="", texthl="DiagnosticSignHint", numhl="DiagnosticSignHint"})
 
 -- set max width of lsp/diagnostic floating windows to 80
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
