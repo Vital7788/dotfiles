e053181ac75235f38c903164891abb32fdb025fd
Vital Wang <vital.wang@hotmail.com>
2026-01-16 21:51:44 +0100

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(proof-general evil sly))
+ '(package-selected-packages '(evil sly))
  '(sly-mrepl-history-file-name "/home/vital/.local/state/sly-mrepl-history"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -22,8 +22,11 @@
   (package-install 'evil))
 
 ;; Enable Evil
+(setq evil-want-Y-yank-to-eol 1)
 (require 'evil)
 (evil-mode 1)
+(evil-set-undo-system 'undo-redo)
+(define-key evil-normal-state-map  (kbd "U") 'evil-redo)
 
 (setq backup-directory-alist '(("." . "~/.local/state/emacs/backup")))
 (set-face-attribute 'default nil :height 120)
@@ -31,3 +34,5 @@
 
 (setq scroll-step 1)
 (setq scroll-margin 1)
+
+(setq inferior-lisp-program "sbcl")
