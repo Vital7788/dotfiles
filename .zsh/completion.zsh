5bf11dd6c457f0239105c09a7b0fd7ec341ecf5f
Vital Wang <vital.wang@hotmail.com>
2026-02-10 00:34:12 +0100

    fzf tab

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -4,7 +4,6 @@ zmodload zsh/complist
 # autoload compinit function and call it to initialize completion for the current session
 autoload -Uz compinit; compinit -d .zsh/zcompdump
 
-
 # Use hjlk in menu selection (during completion)
 # Doesn't work well with interactive mode
 #bindkey -M menuselect 'h' vi-backward-char
@@ -50,22 +49,11 @@ zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' '+l:|=* r:|=*' '
 # limit to len/3 errors
 zstyle ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
 
-
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 # colors for files and directory
 zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # set order of descriptions
 zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
-
-# use ls colors for complist
-zstyle ':completion:*:*:*:*:default' list-colors \
-       ${(s.:.)LS_COLORS}
-
 
 # only display some tags for the command cd
 zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
@@ -87,8 +75,59 @@ zstyle ':completion:*:*:docker-*:*' option-stacking yes
 # replacement="__git_branch_names && ret=0"
 # functions[_git-switch]=${functions[_git-switch]/$current/$replacement}
 
-# only complete local files for performance
-# https://superuser.com/a/459057
-__git_files () {
-    _wanted files expl 'local files' _files
-}
+if [ ! -f ~/.zsh/fzf-tab/fzf-tab.plugin.zsh ]; then
+    zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+    zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
+    zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
+    zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
+    zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+
+    # only complete local files for performance
+    # https://superuser.com/a/459057
+    __git_files () {
+        _wanted files expl 'local files' _files
+    }
+else
+    source ~/.zsh/fzf-tab/fzf-tab.plugin.zsh
+    FZF_GIT_DEFAULT_FLAGS=("--height=50%"
+        "--layout=reverse" "--multi" "--min-height=20+" "--border"
+        "--no-separator" "--header-border=horizontal"
+        "--preview-window=right,50%" "--preview-border=line"
+        "--bind=ctrl-/:change-preview-window(down,50%|hidden|)"
+    )
+
+    GIT_PREVIEW_FILE='git -c core.quotePath=false diff --no-ext-diff --color=always -- $realpath | delta; bat --style=full --color=always --pager=never $realpath'
+    GIT_PREVIEW_HASH='git show --color=always --decorate $word | delta'
+    GIT_PREVIEW_BRANCH="git log --oneline --graph --date=short --color=always --pretty='format:%C(auto)%cd %h%d %s' \$word"
+
+    # set descriptions format to enable group support
+    zstyle ':completion:*:descriptions' format '[%d]'
+    zstyle ':fzf-tab:*' fzf-flags --height=40%
+    zstyle ':fzf-tab:*' use-fzf-default-opts yes
+    zstyle ':fzf-tab:*' switch-group '<' '>'
+    zstyle ':fzf-tab:*' prefix ''
+    zstyle ':fzf-tab:*' single-group header
+
+    zstyle ':fzf-tab:complete:git-*:*' fzf-flags "${FZF_GIT_DEFAULT_FLAGS[@]}"
+    zstyle ':fzf-tab:complete:git-help:*' fzf-preview \
+            'git help $word | bat -plman --color=always'
+    zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview "$GIT_PREVIEW_FILE"
+    zstyle ':fzf-tab:complete:git-show:*' fzf-flags "${FZF_GIT_DEFAULT_FLAGS[@]}"
+    zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+        "case \$group in
+        '[cached file]')
+            $GIT_PREVIEW_FILE
+            ;;
+        *)
+            $GIT_PREVIEW_HASH
+            ;;
+        esac"
+
+    zstyle ':fzf-tab:complete:git-(log|switch|branch):*' fzf-preview "$GIT_PREVIEW_BRANCH"
+    zstyle ':fzf-tab:complete:git-switch:*' fzf-flags \
+        "${FZF_GIT_DEFAULT_FLAGS[@]}" \
+        --tiebreak begin \
+        --preview-window down,border-top,40% \
+        --color hl:underline,hl+:underline \
+        --no-hscroll
+fi
