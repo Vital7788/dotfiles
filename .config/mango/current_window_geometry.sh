ec88c5a135c8217f5feb04f9e97142cf7c4f4564
Vital Wang <vital.wang@hotmail.com>
2026-03-14 17:15:22 +0100

    mango layout

--- .config/mango/current_window_geometry.sh
+++ .config/mango/current_window_geometry.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # look for line where third column is 1, then print first column
-current_monitor=$(mmsg -g -o | awk '$3 == 1 { print $1 }')
+current_output=$(mmsg -g -o | awk '$3 == 1 { print $1 }')
 
-# create variable `monitor`, filter lines that start with `monitor`
-# build an associative array `val`, and finally format with printf
-mmsg -g -x | awk -v monitor="$current_monitor" '$1 == monitor { val[$2] = $3 } END { printf "%s,%s %sx%s\n", val["x"], val["y"], val["width"], val["height"] }'
+# build an associative array `geometry` and format with printf
+mmsg -o "$current_output" -g -x | awk '{ geometry[$1] = $2 } END { printf "%s,%s %sx%s\n", geometry["x"], geometry["y"], geometry["width"], geometry["height"] }'
--- .config/mango/mode.sh
+++ .config/mango/mode.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# note: this is not accurate if not all monitors have the same mode
-mmsg -w -b | while read monitor _ mode
+mmsg -o "$WAYBAR_OUTPUT_NAME" -w -b | while read _ mode
 do
     case $mode in
         exit)
             echo "exit: loc[k], [l]ogout, s[u]spend, [r]eboot, [s]hutdown"
             ;;
-        screenshot)
-            echo "a:󰹑 , s:󰩭 , d:✎󰹑 , f:✎󰩭 , g:✎󰖯 , e: 󰹑 , r: 󰩭 "
+        layout)
+            current_layout=$(mmsg -o "$WAYBAR_OUTPUT_NAME" -g -l | awk '{ print $2 }')
+            echo "layout [$current_layout]: [c]enter_tile, [t]ile, [r]ight_tile, [s]croller, [d]eck, [v]ertical_deck, [n]ext layout"
+            ;;
+        screencopy)
+            echo "copy screen: a:󰹑 , s:󰩭 , d:✎󰹑 , f:✎󰩭 , g:✎󰖯 , e: 󰹑 , r: 󰩭 "
             ;;
         default)
             echo ""
