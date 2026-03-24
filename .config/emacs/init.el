b9db517e78f3d144b9165f73893e54a414829be5
Vital Wang <vital.wang@hotmail.com>
2026-03-24 14:30:05 +0100

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,43 +1,66 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil evil-collection magit sly))
- '(sly-mrepl-history-file-name "/home/vital/.local/state/sly-mrepl-history"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
 
-;; Enable Evil
-(setq evil-want-keybinding nil)
-(setq evil-want-Y-yank-to-eol 1)
-(require 'evil)
-(evil-mode 1)
-(evil-set-undo-system 'undo-redo)
-(define-key evil-normal-state-map  (kbd "U") 'evil-redo)
-(when (require 'evil-collection nil t)
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-want-Y-yank-to-eol 1)
+  :config
+  (evil-mode 1)
+  (evil-set-undo-system 'undo-redo)
+  (define-key evil-normal-state-map  (kbd "U") 'evil-redo))
+(use-package evil-collection
+  :ensure t)
+(use-package magit
+  :ensure t
+  :config
   (evil-collection-init))
+(use-package sly
+  :ensure nil
+  :config
+  (setq sly-mrepl-history-file-name "/home/vital/.local/state/sly-mrepl-history")
+  (setq inferior-lisp-program "sbcl"))
+
+(defun prot/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
 
 (load-theme 'modus-operandi-tinted)
 
+(let ((mono-spaced-font "FiraCode Nerd Font Mono")
+      (proportionately-spaced-font "FiraCode Nerd Font Propo"))
+  (set-face-attribute 'default nil :family mono-spaced-font :height 108)
+  (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
+  (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))
+
 (setq backup-directory-alist '(("." . "~/.local/state/emacs/backup")))
-(set-face-attribute 'default nil :height 120)
 (show-paren-mode 1)
 
 (setq scroll-step 1)
 (setq scroll-margin 1)
-
-(setq inferior-lisp-program "sbcl")
