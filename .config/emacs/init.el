4421829bec412a613fb92328ea4234b5948758aa
Vital Wang <vital.wang@hotmail.com>
2026-03-24 22:26:53 +0100

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,85 +1,125 @@
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file :no-error-if-file-is-missing)
 
+;;; Set up package manager
+
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(use-package dash
-  :ensure t)
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-keybinding nil)
-  (setq evil-want-Y-yank-to-eol 1)
-  :config
-  (evil-mode 1)
-  (evil-set-undo-system 'undo-redo)
-  (define-key evil-normal-state-map  (kbd "U") 'evil-redo)
-  (defun my/magit-process-environment (env)
-    "Detect and set git -bare repo env vars when in tracked dotfile directories."
-    (let* ((default (file-name-as-directory (expand-file-name default-directory)))
-           (git-dir (expand-file-name "~/.cfg"))
-           (work-tree (expand-file-name "~/"))
-           (dotfile-dirs
-            (-map (apply-partially 'concat work-tree)
-                  (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
-                  (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
-                          git-dir work-tree))))))))
-      (push work-tree dotfile-dirs)
-      (when (member default dotfile-dirs)
-        (push (format "GIT_WORK_TREE=%s" work-tree) env)
-        (push (format "GIT_DIR=%s" git-dir) env)))
-  env)
- (advice-add 'magit-process-environment
-             :filter-return #'my/magit-process-environment))
-(use-package evil-collection
-  :ensure t
-  :config
-  (evil-collection-init))
-(use-package magit
-  :ensure t)
-(use-package sly
-  :ensure nil
-  :config
-  (setq sly-mrepl-history-file-name "/home/vital/.local/state/sly-mrepl-history")
-  (setq inferior-lisp-program "sbcl"))
 
-(defun prot/keyboard-quit-dwim ()
-  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+;; Don't show byte compilation warnings after installing packages
+(add-to-list 'display-buffer-alist
+             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
+               (display-buffer-no-window)
+               (allow-no-window . t)))
 
-The generic `keyboard-quit' does not do the expected thing when
-the minibuffer is open.  Whereas we want it to close the
-minibuffer, even without explicitly focusing it.
-
-The DWIM behaviour of this command is as follows:
-
-- When the region is active, disable it.
-- When a minibuffer is open, but not focused, close the minibuffer.
-- When the Completions buffer is selected, close it.
-- In every other case use the regular `keyboard-quit'."
-  (interactive)
-  (cond
-   ((region-active-p)
-    (keyboard-quit))
-   ((derived-mode-p 'completion-list-mode)
-    (delete-completion-window))
-   ((> (minibuffer-depth) 0)
-    (abort-recursive-edit))
-   (t
-    (keyboard-quit))))
-
-(define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
-
-(load-theme 'modus-operandi-tinted)
-
-(let ((mono-spaced-font "FiraCode Nerd Font Mono")
-      (proportionately-spaced-font "FiraCode Nerd Font Propo"))
-  (set-face-attribute 'default nil :family mono-spaced-font :height 108)
-  (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
-  (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))
+;;; Basic behavior
 
 (setq backup-directory-alist '(("." . "~/.local/state/emacs/backup")))
 (show-paren-mode 1)
 
 (setq scroll-step 1)
 (setq scroll-margin 1)
+
+;;; Appearance
+
+(load-theme 'modus-operandi-tinted)
+
+(let ((mono-spaced-font "FiraCode Nerd Font")
+      (proportionately-spaced-font "FiraCode Nerd Font Propo"))
+  (set-face-attribute 'default nil :family mono-spaced-font :height 100 :weight 'light)
+  (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
+  (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))
+
+;;; Evil
+
+(use-package dash
+  :ensure t)
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-want-Y-yank-to-eol 1)
+  :config
+  (evil-set-undo-system 'undo-redo)
+  (define-key evil-normal-state-map (kbd "U") 'evil-redo)
+  (defun my/magit-process-environment (env)
+    "Detect and set git -bare repo env vars when in tracked dotfile directories."
+    (let* ((default (file-name-as-directory (expand-file-name default-directory)))
+	   (git-dir (expand-file-name "~/.cfg"))
+	   (work-tree (expand-file-name "~/"))
+	   (dotfile-dirs
+	    (-map (apply-partially 'concat work-tree)
+		  (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
+								     (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
+									     git-dir work-tree))))))))
+      (push work-tree dotfile-dirs)
+      (when (member default dotfile-dirs)
+	(push (format "GIT_WORK_TREE=%s" work-tree) env)
+	(push (format "GIT_DIR=%s" git-dir) env)))
+    env)
+  (advice-add 'magit-process-environment
+	      :filter-return #'my/magit-process-environment)
+  :hook (after-init . evil-mode))
+
+(use-package evil-collection
+  :ensure t
+  :hook (after-init . evil-collection-init))
+
+(use-package evil-commentary
+  :ensure t
+  :hook (after-init . evil-commentary-mode))
+
+;;; Minibuffer
+;; More advanced stuff here: https://protesilaos.com/codelog/2024-02-17-emacs-modern-minibuffer-packages/
+
+(use-package vertico
+  :ensure t
+  :hook (after-init . vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  :hook (after-init . marginalia-mode))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides nil))
+
+(use-package savehist
+  :ensure nil ; it is built-in
+  :hook (after-init . savehist-mode))
+
+;;; File manager (Dired)
+
+(use-package dired
+  :ensure nil
+  :commands (dired)
+  :hook
+  ((dired-mode . dired-hide-details-mode)
+   (dired-mode . hl-line-mode))
+  :config
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'always))
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>" . dired-subtree-toggle)
+    ("TAB" . dired-subtree-toggle)))
+
+;;; Magit
+(use-package magit
+  :ensure t)
+
+;;; Language specific
+(use-package sly
+  :ensure nil
+  :config
+  (setq sly-mrepl-history-file-name "/home/vital/.local/state/sly-mrepl-history")
+  (setq inferior-lisp-program "sbcl"))
