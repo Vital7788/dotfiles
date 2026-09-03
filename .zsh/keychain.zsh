59f521002a8eead6c2f2b24ee6a5453345ab8608
Vital Wang <vital.wang@hotmail.com>
2026-09-03 08:13:47 +0200

    keychain legacy compatibility

--- .zsh/keychain.zsh
+++ .zsh/keychain.zsh
@@ -1,11 +1,13 @@
 if (( $+commands[keychain] )); then
     keychain() {
-        command keychain --theme legacy --absolute --dir $XDG_RUNTIME_DIR/keychain "$@"
+        # command keychain --theme legacy --absolute --dir $XDG_RUNTIME_DIR/keychain "$@"
+        command keychain --absolute --dir $XDG_RUNTIME_DIR/keychain "$@"
     }
 
     () {
         local pidfile=$XDG_RUNTIME_DIR/keychain/$HOST-sh
         [[ -r $pidfile ]] && source $pidfile
-        [[ -S ${SSH_AUTH_SOCK-} ]] || eval "$(keychain agent start --eval --quiet)"
+        # [[ -S ${SSH_AUTH_SOCK-} ]] || eval "$(keychain agent start --eval --quiet)"
+        [[ -S ${SSH_AUTH_SOCK-} ]] || eval "$(keychain --eval --quiet)"
     }
 fi
